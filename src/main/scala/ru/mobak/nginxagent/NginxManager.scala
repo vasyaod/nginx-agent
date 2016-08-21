2e87c4f3591729595cafd44f4076342ff72a1acb
Vasiliy Vazhesov <vasiliy.vazhesov@gmail.com>
2016-08-21 20:21:44 +0500

    Some kind of cool down time was added in Nginx restart

--- src/main/scala/ru/mobak/nginxagent/NginxManager.scala
+++ src/main/scala/ru/mobak/nginxagent/NginxManager.scala
@@ -1,8 +1,10 @@
 package ru.mobak.nginxagent
 
-import akka.actor.Actor
+import akka.actor.{Actor, Cancellable}
 import com.typesafe.scalalogging.LazyLogging
+
 import sys.process._
+import scala.concurrent.duration._
 
 object NginxManager {
   case object Refresh
@@ -11,8 +13,20 @@ object NginxManager {
 /** The actor force Nginx to refresh configuration. */
 class NginxManager(pidFile: String) extends Actor with LazyLogging {
 
+  /** Command for server restarting */
+  case object Restart
+
+  var task: Option[Cancellable] = None
+
   override def receive: Receive = {
     case NginxManager.Refresh =>
+
+      implicit val ec = context.dispatcher
+
+      task.foreach(_.cancel())
+      task = Some(context.system.scheduler.scheduleOnce(1.seconds, self, Restart))
+
+    case Restart =>
       logger.info("Nginx configuration refreshing.")
       val pid = scala.io.Source.fromFile(pidFile).mkString
       logger.info(s"Nginx pid: $pid")
